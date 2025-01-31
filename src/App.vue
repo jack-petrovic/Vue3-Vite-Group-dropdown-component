cda332e1894f3bed9d18f12ce2289bef4828ecf8
Admin <jack.petrovic@outlook.com>
2025-01-31 10:40:09 +0100

    initialize component.

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <div class="max-w-2xl mx-auto px-4 py-5 pt-10">
+      <Dropdown
+        placeholder="Select Account"
+        v-model="selectedValue"
+        :options="
+              [
+                  { label: 'Option A', value: 'valA', group: 'Group A' },
+                  { label: 'Option B', value: 'valB', group: 'Group B' },
+                  { label: 'Option C', value: 'valC', group: 'Group A' }
+              ]"
+        :groupBy="(option) => option.group"
+      />
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import Dropdown from './components/Dropdown.vue';
+
+const selectedValue = ref('');
+
+</script>
